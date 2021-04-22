5634373b2722dc7847df386fad626c58d3cadd03
Jesus Castro Olaya <programador3@email.cloudnets.co>
2021-04-22 11:58:55 -0500

    feat: agregando querys para el crud

--- src/models/Categorias/query.js
+++ src/models/Categorias/query.js
@@ -1,7 +1,9 @@
 //Consulta para Listar las categorias
-export const getCategorias = `SELECT * FROM categorias `;
+export const getCategorias = `SELECT * FROM categorias  ORDER BY id DESC`;
 //Consulta para agregar categoria
 export const addCategoria = `INSERT INTO categorias (nombre_categoria)
 VALUES ($1)`;
-
-
+//Consulta para actualizar categoria
+export const editCategoria = `UPDATE categorias SET nombre_categoria=$1 WHERE id=$2`;
+//Consulta para eliminar categoria
+export const deleteCategoria = "DELETE FROM categorias WHERE id=$1";
--- src/models/articulos/querys.js
+++ src/models/articulos/querys.js
@@ -1,1 +1,9 @@
-export const getArticulos = `SELECT * FROM articulos `;
+//Listado de articulos
+export const getArticulos = `SELECT * FROM articulos ORDER BY id DESC`;
+//Consulta para agregar articulos
+export const addArticulos = `INSERT INTO articulos (numero_registro,nombre,descripcion)
+VALUES ($1,$2,$3)`;
+//Consulta para actualizar categoria
+export const editArticulos = `UPDATE articulos SET numero_registro=$1,nombre=$2,descripcion=$3 WHERE id=$4`;
+//Consulta para eliminar categoria
+export const deleteArticulos = "DELETE FROM articulos WHERE id=$1";
